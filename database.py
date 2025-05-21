d7c473814e6480612e86429fc34b25d412f8868d
Tim <timaka710@gmail.com>
2025-05-21 21:42:35 +0300

    Добавлена поддержка SSL-сертификата

--- database.py
+++ database.py
@@ -7,10 +7,11 @@ load_dotenv()
 
 # Функция для подключения к базе данных
 def get_db():
-    """Возвращает подключение к MySQL."""
+    """Возвращает подключение к MySQL с поддержкой SSL."""
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        ssl_ca="/root/gba_bot/ca-certificate.crt"  # путь к сертификату на сервере
     )
