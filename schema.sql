9ef1b60747055c98b6c9033d4161de0408235413
frien9zone <timaka710@gmail.com>
2025-06-05 02:19:51 +0300

    schema for virtual server

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS test_drivers.driver_info (
+CREATE TABLE IF NOT EXISTS defaultdb.driver_info (
     driver_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(60) NOT NULL,
     phone_number VARCHAR(20),
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS test_drivers.driver_info (
     current_day_of_week TEXT
 );
 
-CREATE TABLE IF NOT EXISTS test_drivers.trailer_info (
+CREATE TABLE IF NOT EXISTS defaultdb.trailer_info (
     trailer_id INT AUTO_INCREMENT PRIMARY KEY,
     driver_id INT NOT NULL,
     trailer_type TEXT,
@@ -16,6 +16,6 @@ CREATE TABLE IF NOT EXISTS test_drivers.trailer_info (
     special_equipment TEXT,
     MC TEXT,
 
-    FOREIGN KEY (driver_id) REFERENCES test_drivers.driver_info(driver_id)
+    FOREIGN KEY (driver_id) REFERENCES defaultdb.driver_info(driver_id)
         ON DELETE CASCADE
 );
