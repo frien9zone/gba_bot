818c8c56681f69289f6f25f370b480f9d7b51bf2
frien9zone <timaka710@gmail.com>
2025-06-05 02:13:15 +0300

    Schema + main ready for MC

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE test_drivers.driver_info (
+CREATE TABLE IF NOT EXISTS test_drivers.driver_info (
     driver_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(60) NOT NULL,
     phone_number VARCHAR(20),
@@ -7,14 +7,14 @@ CREATE TABLE test_drivers.driver_info (
     current_day_of_week TEXT
 );
 
-CREATE TABLE test_drivers.trailer_info (
+CREATE TABLE IF NOT EXISTS test_drivers.trailer_info (
     trailer_id INT AUTO_INCREMENT PRIMARY KEY,
     driver_id INT NOT NULL,
     trailer_type TEXT,
     length INT,
     bee_nets TEXT,
     special_equipment TEXT,
-    MC INT,
+    MC TEXT,
 
     FOREIGN KEY (driver_id) REFERENCES test_drivers.driver_info(driver_id)
         ON DELETE CASCADE
